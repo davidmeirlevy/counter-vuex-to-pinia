2d8f68a1c122ec042a00f0722626dff2cdc5b253
David Meir-Levy <davidmeirlevy@gmail.com>
2022-03-15 15:14:02 +0200

    initial commit

---
+++ Counter.vue
@@ -0,0 +1,35 @@
+<template>
+	<div id="app">
+		Clicked: {{ count }} times, count is {{ evenOrOdd }}.
+		<CounterActions/>
+	</div>
+</template>
+
+<script>
+import { computed } from 'vue'
+import { useStore } from 'vuex'
+import CounterActions from './CounterActions.vue';
+
+export default {
+	components: { CounterActions },
+	setup() {
+		const store = useStore()
+
+		return {
+			count: computed(() => store.state.count),
+			evenOrOdd: computed(() => store.getters.evenOrOdd),
+			increment: () => store.dispatch('increment'),
+			decrement: () => store.dispatch('decrement'),
+			incrementIfOdd: () => store.dispatch('incrementIfOdd'),
+			incrementAsync: () => store.dispatch('incrementAsync')
+		}
+	}
+}
+</script>
+<style>
+body {
+		background-color: #000;
+		color: #fff;
+		font-size: 3em;
+}
+</style>
---
+++ CounterActions.vue
@@ -0,0 +1,34 @@
+<template>
+	<div>
+		<button @click="increment">+</button>
+		<button @click="decrement">-</button>
+		<button @click="incrementIfOdd">Increment if odd</button>
+		<button @click="incrementAsync">Increment async</button>
+	</div>
+</template>
+
+<script>
+import { computed } from 'vue'
+import { useStore } from 'vuex'
+
+export default {
+	setup() {
+		const store = useStore()
+
+		return {
+			evenOrOdd: computed(() => store.getters.evenOrOdd),
+			increment: () => store.dispatch('increment'),
+			decrement: () => store.dispatch('decrement'),
+			incrementIfOdd: () => store.dispatch('incrementIfOdd'),
+			incrementAsync: () => store.dispatch('incrementAsync')
+		}
+	}
+}
+</script>
+<style scoped>
+button {
+		font-size: 1em;
+		margin: 0.2em;
+		padding: 0.5em;
+}
+</style>
